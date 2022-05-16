f59c9a97b28f098c551355182f12c5de1dc41896
Alamin-git <alamin2.mas@gmail.com>
2022-05-17 02:08:09 +0600

    change folder name

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
-
+// middleware
 app.use(cors());
 app.use(express());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqpuq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("emaJhon").collection("product");
+
+    // for all products
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } 
+  finally {}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
